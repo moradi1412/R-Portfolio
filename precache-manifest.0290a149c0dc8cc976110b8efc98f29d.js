91f807b424b8402983e260f448c744fd38bcdb87
Moradi1412 <moradi.javid@gmail.com>
2021-11-28 18:00:24 -0800

    Updates

---
+++ precache-manifest.0290a149c0dc8cc976110b8efc98f29d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e24a6685251b0e0fa3c5137a9a0748cd",
+    "url": "/R-Portfolio/index.html"
+  },
+  {
+    "revision": "305d528b50f467bbd854",
+    "url": "/R-Portfolio/static/css/2.fa43bc9e.chunk.css"
+  },
+  {
+    "revision": "24d013500c3cddb2370f",
+    "url": "/R-Portfolio/static/css/main.4622ef28.chunk.css"
+  },
+  {
+    "revision": "305d528b50f467bbd854",
+    "url": "/R-Portfolio/static/js/2.8c8b1c30.chunk.js"
+  },
+  {
+    "revision": "119ff7b0e6dbb852f63360f69938dd73",
+    "url": "/R-Portfolio/static/js/2.8c8b1c30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69aed9fe0cab7deeb6f6",
+    "url": "/R-Portfolio/static/js/3.5d77e048.chunk.js"
+  },
+  {
+    "revision": "24d013500c3cddb2370f",
+    "url": "/R-Portfolio/static/js/main.8b49b831.chunk.js"
+  },
+  {
+    "revision": "7dc19deb3340fb126d65",
+    "url": "/R-Portfolio/static/js/runtime-main.731be5e7.js"
+  },
+  {
+    "revision": "76c28839fb14d1865bc9d307261e79b5",
+    "url": "/R-Portfolio/static/media/2.76c28839.jpg"
+  },
+  {
+    "revision": "9b4f92f8b24112222789c0c1cacbc3b4",
+    "url": "/R-Portfolio/static/media/Mohammad.9b4f92f8.pdf"
+  },
+  {
+    "revision": "e40c3958debbb0a3b2af8548a2b31cc6",
+    "url": "/R-Portfolio/static/media/background.e40c3958.jpg"
+  }
+]);
